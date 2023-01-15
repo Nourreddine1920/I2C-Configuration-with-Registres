dcb6c524e2d6bffef44d8010e62aff1015dd6f39
Nourreddine1920 <nourreddine2000@gmail.com>
2023-01-15 21:21:59 +0100

    Configure the I2C PINs for ALternate Functions

--- I2C.c
+++ I2C.c
@@ -10,6 +10,16 @@ void I2C_Config (void){
 
     // Configure the I2C PINs for ALternate Functions
 	GPIOB->MODER |= (2<<12) | (2<<14);  // Bits (13:12)= 1:0 --> Alternate Function for Pin PB6; Bits (15:14)= 1:0 --> Alternate Function for Pin PB7
+    GPIOB->OTYPER |= (1<<6) | (1<<7);  //  Bit6=1, Bit7=1  output open drain
+    GPIOB->OSPEEDR |= (2<<12) | (2<<14);  // Bits (13:12)= 1:0 --> High Speed for PIN PB6; Bits (15:14)= 1:1 --> High Speed for PIN PB7
+    GPIOB->PUPDR |= (1<<12) | (1<<14);  // Bits (13:12)= 0:1 --> Pull up for PIN PB6; Bits (15:14)= 0:1 --> pull up for PIN PB7
+    GPIOB->AFR[0] |= (4<<24) | (4<<28);  // Bits (24:25:26:27) = 0:1:0:0 --> AF4 for pin PB6;  Bits (28:29:30:31) = 0:1:0:0 --> AF4 for pin PB7
+
+    // Reset the I2C 
+	I2C1->CR1 |= (1<<15);
+	I2C1->CR1 &= ~(1<<15);
+
+
 
     
 }
