a6928617b786950626a6a402281c60eb043f8213
Nourreddine1920 <nourreddine2000@gmail.com>
2023-01-15 18:51:44 +0100

    config GPIO

--- I2C.c
+++ I2C.c
@@ -7,5 +7,9 @@ void I2C_Config (void){
 	RCC->APB1ENR |= (1<<21);  // enable I2C CLOCK
 	RCC->AHB1ENR |= (1<<1);  // Enable GPIOB CLOCK
 
+
+    // Configure the I2C PINs for ALternate Functions
+	GPIOB->MODER |= (2<<12) | (2<<14);  // Bits (13:12)= 1:0 --> Alternate Function for Pin PB6; Bits (15:14)= 1:0 --> Alternate Function for Pin PB7
+
     
 }
